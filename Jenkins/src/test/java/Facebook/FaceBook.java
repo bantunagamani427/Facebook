79a04c7039e22f28ad1932890cbd0c3bb7ac1680
bantunagamani427 <bantunagamani427>
2022-08-02 13:30:13 +0530

    commit to testng fb

--- Jenkins/src/test/java/Facebook/FaceBook.java
+++ Jenkins/src/test/java/Facebook/FaceBook.java
@@ -1,11 +1,19 @@
 package Facebook;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class FaceBook {
 @Test
 public void facebook() {
-System.out.println("hi this is FaceBook");
+	WebDriverManager.chromedriver().setup();
+	WebDriver driver=new ChromeDriver();
+	driver.get("https://www.facebook.com/");
+	String fb = driver.getCurrentUrl();
+System.out.println(fb);
 	}
 
 }
